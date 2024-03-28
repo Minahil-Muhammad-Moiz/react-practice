069149c3e0de43e4a6aea3723daf4b1287342bfb
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2024-03-29 00:44:06 +0500

    To do List

---
+++ src/Components/ToDoList.jsx
@@ -0,0 +1,14 @@
+const ToDoList = () => {
+  return (
+    <>
+        <h1>To Do List</h1>
+        <div className='AddTask'>
+            <input/>
+            <button>Add Task</button>
+        </div>
+        <div className='List'></div>
+    </>
+  )
+}
+
+export default ToDoList
