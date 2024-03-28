07157f0f1955e15d47e2aa2d887e9e8c39b47d72
Minahil-Muhammad-Moiz <minahil4864@gmail.com>
2024-03-28 22:34:49 +0500

    useSate Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
 import Planets from './Components/Planets'
 import User from './Components/User'
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const increaseCount = ()=>{
+    setCount(count+1)
+  }
   return (
     <>
     <div>
@@ -11,6 +17,13 @@ function App() {
     </div>
     <div>
       {Planets.map((planet, key)=> !planet.isGasPlanet && <h1>{planet.name}</h1>)}
+    </div>
+    <div className='uCard'>
+      <h1>Basic Counter</h1>
+      <h1>{count}</h1>
+      <br/>
+      <button onClick={increaseCount}>Count</button>
+      <button onClick={()=> setCount(0)}>Reset</button>
     </div>
     </>
   )
