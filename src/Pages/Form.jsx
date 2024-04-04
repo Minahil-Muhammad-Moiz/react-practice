56c0fa491fb71e460b1bce781171fb28b657f960
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-04 10:49:00 +0500

    Shaping form data using YUP

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -1,7 +1,16 @@
 import {useForm} from 'react-hook-form'
+import * as yup from "yup";
 
 const Form = () => {
     const {register, handleSubmit} = useForm()
+
+    const schema = yup.object().shape({
+      fullname: yup.string().required('Fullname is required'),
+      Age: yup.number().integer().positive().min(18).required('Age is required'),
+      Email: yup.email().required('Email is required'),
+      password: yup.string().min(4).max(10).required(),
+      confirmPassword: yup.string().oneOf([yup.ref("password")],"Passwords must match"),
+    })
 
     const formData = (data)=>{
         console.log(data)
