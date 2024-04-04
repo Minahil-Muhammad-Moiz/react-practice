fabe578146379e2bd9c2907091708b1acfac0871
Minahil Muhammad Moiz <minahil4864@gmail.com>
2024-04-04 11:11:20 +0500

    add error messages with form inputs

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -1,30 +1,38 @@
-import {useForm} from 'react-hook-form'
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 const Form = () => {
-    const {register, handleSubmit} = useForm()
+  const schema = yup.object().shape({
+    fullname: yup.string().required("Fullname is required"),
+    Age: yup.number().integer().positive().min(18).required('Age is required'),
+    Email: yup.string().email().required("Email is required"),
+    password: yup.string().min(4).max(10).required(),
+    confirmPassword: yup.string().oneOf([yup.ref("password")], "Passwords must match"),
+  });
 
-    const schema = yup.object().shape({
-      fullname: yup.string().required('Fullname is required'),
-      Age: yup.number().integer().positive().min(18).required('Age is required'),
-      Email: yup.email().required('Email is required'),
-      password: yup.string().min(4).max(10).required(),
-      confirmPassword: yup.string().oneOf([yup.ref("password")],"Passwords must match"),
-    })
+  const { register, handleSubmit, formState: {errors} } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-    const formData = (data)=>{
-        console.log(data)
-    }
+  const formData = (data) => {
+    console.log(data);
+  };
   return (
     <form onSubmit={handleSubmit(formData)}>
-        <input type="text" placeholder="Full Name..." {...register("fullname")}/>
-        <input type="number" placeholder="Age..." {...register("Age")}/>
-        <input type="email" placeholder="Email..." {...register("Email")}/>
-        <input type="password" placeholder="password" {...register("password")}/>
-        <input type="password" placeholder="confirm password" />
-        <input type="submit" />
+      <input type="text" placeholder="Full Name..." {...register("fullname")} />
+      <p>{errors.fullname?.message}</p>
+      <input type="number" placeholder="Age..." {...register("Age")} />
+      <p>{errors.Age?.message}</p>
+      <input type="email" placeholder="Email..." {...register("Email")} />
+      <p>{errors.Email?.message}</p>
+      <input type="password" placeholder="password" {...register("password")} />
+      <p>{errors.password?.message}</p>
+      <input type="password" placeholder="confirm password" {...register("confirmPassword")}/>
+      <p>{errors.confirmPassword?.message}</p>
+      <input type="submit" />
     </form>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
